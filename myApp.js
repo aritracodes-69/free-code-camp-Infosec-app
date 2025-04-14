146d9e59c741919b8eb4ae07be743fd5c249f261
Aritra Ghosh <rooster@rooster.lan>
2025-04-14 12:56:37 +0530

    Used Yarn and Fixed Double Listening Errors

--- myApp.js
+++ myApp.js
@@ -2,10 +2,7 @@ const express = require('express');
 const helmet = require('helmet');
 const app = express();
 
-const PORT = process.env.PORT||3030;
 
-app.listen(PORT, () => {console.log("Useful programmer security server started ${PORT}");
-});
 
 
 
@@ -60,6 +57,5 @@ app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 let port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Your app is listening on port ${port}`);
+app.listen(port, () => {console.log("Useful programmer security server started ${port}");
 });
